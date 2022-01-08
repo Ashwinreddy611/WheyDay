08de492e4c1791a5a79c1b024e1ec31b9153eafe
Ashwinreddy611 <ashwinreddy6@gmail.com>
2022-01-08 18:49:26 +0000

    carousel images now showing and formatted for smaller screens also

--- wheyday/settings.py
+++ wheyday/settings.py
@@ -69,7 +69,8 @@ TEMPLATES = [
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth', #required by aullauth
+                # required by aullauth
+                'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
         },
