c2baa27b91fc6991409ff2670e614542365e8564
Ashwinreddy611 <ashwinreddy6@gmail.com>
2022-01-09 15:36:23 +0000

    products now availiable to be seen and populated on site, delete and edit functions created also

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from .models import Product
 from .forms import ProductForm
 
@@ -30,11 +30,50 @@ def product_detail(request, product_id):
 
 
 def add_product(request):
-    """ Addition of products to the site """
-    form = ProductForm()
+    """ Add a product to the store """
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            product = form.save()
+            return redirect(reverse('product_detail', args=[product.id]))
+        else:
+            return redirect('products.html')
+    else:
+        form = ProductForm()
+
     template = 'products/add_product.html'
     context = {
         'form': form,
     }
 
     return render(request, template, context)
+
+
+def edit_product(request, product_id):
+    """ Edit a product in the site """
+    product = get_object_or_404(Product, pk=product_id)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse('product_detail', args=[product.id]))
+        else:
+            return redirect('products.html')
+    else:
+        form = ProductForm(instance=product)
+
+    template = 'products/edit_product.html'
+    context = {
+        'form': form,
+        'product': product,
+    }
+
+    return render(request, template, context)
+
+
+def delete_product(request, product_id):
+    """ Delete a product from the site """
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+
+    return redirect(reverse('products.html'))
