1485b5a066d8d7770992d24d489ac153ecfb545c
Ashwinreddy611 <ashwinreddy6@gmail.com>
2022-06-27 17:58:25 +0000

    fixed error for non logged in users when checking out

--- checkout/views.py
+++ checkout/views.py
@@ -69,12 +69,12 @@ def checkout(request):
                         )
                         order_line_item.save()
                     else:
-                        for size, quantity in item_data['items_by_size'].items():
+                        for flavour, quantity in item_data['items_by_flavour'].items():
                             order_line_item = OrderLineItem(
                                 order=order,
                                 product=product,
                                 quantity=quantity,
-                                product_size=size,
+                                product_flavour=flavour,
                             )
                             order_line_item.save()
                 except Product.DoesNotExist:
